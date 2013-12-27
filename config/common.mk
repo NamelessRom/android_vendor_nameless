45a640a331c91f18f7e48076905d133100aaaa78
Alexander Martinz <eviscerationls@gmail.com>
2013-12-27 14:55:22 +0100

    Initial Vendor Setup

---
+++ config/common.mk
@@ -0,0 +1,54 @@
+PRODUCT_BRAND ?= nameless
+
+# bootanimation
+PRODUCT_COPY_FILES += \
+	vendor/nameless/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+
+# general properties
+PRODUCT_PROPERTY_OVERRIDES += \
+	keyguard.no_require_sim=true \
+	ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+	ro.com.google.clientidbase=android-google \
+	ro.com.android.wifi-watchlist=GoogleGuest \
+	ro.setupwizard.enterprise_mode=1 \
+	ro.com.android.dateformat=MM-dd-yyyy \
+	ro.com.android.dataroaming=false \
+	persist.sys.root_access=1
+
+# enable ADB authentication if not on eng build
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+endif
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/nameless/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/nameless/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/nameless/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
+    vendor/nameless/prebuilt/bin/blacklist:system/addon.d/blacklist
+
+# init.d support
+PRODUCT_COPY_FILES += \
+	vendor/nameless/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
+	vendor/nameless/prebuilt/bin/sysinit:system/bin/sysinit
+
+# userinit support
+PRODUCT_COPY_FILES += \
+    vendor/nameless/prebuilt/etc/init.d/90userinit:system/etc/init.d/90userinit
+
+# Init script file with nameless extras
+PRODUCT_COPY_FILES += \
+    vendor/nameless/prebuilt/etc/init.local.rc:root/init.nameless.rc
+
+# Enable SIP and VoIP on all targets
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Additional packages
+-include vendor/nameless/config/packages.mk
+
+# Versioning
+-include vendor/nameless/config/version.mk
+
+# Add our overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/nameless/overlay/common
