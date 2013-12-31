981299ba3b723b59ff4cb31d12c07e067393d335
Alexander Martinz <eviscerationls@gmail.com>
2013-12-31 16:07:30 +0100

    move superuser flags
    Change-Id: Iae17ba7f7db45fa7b68f43dd44cf901d55ac7081

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,9 @@
 PRODUCT_BRAND ?= nameless
 
+# Superuser
+SUPERUSER_EMBEDDED := true
+SUPERUSER_PACKAGE_PREFIX := com.android.settings.cyanogenmod.superuser
+
 # bootanimation
 PRODUCT_COPY_FILES += \
 	vendor/nameless/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
