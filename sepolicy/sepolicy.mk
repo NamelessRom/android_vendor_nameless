f5ce03af7cfaf49f0fc7f949cc6dd8b2c4d1c9c8
Alexander Martinz <eviscerationls@gmail.com>
2014-01-11 01:00:02 +0100

    import sepolicy from cyanogenmod
    Change-Id: I1cb2089f641995a8ef532c00ee64cc83f2fcfc6d

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -7,4 +7,11 @@ BOARD_SEPOLICY_DIRS += \
     vendor/nameless/sepolicy
 
 BOARD_SEPOLICY_UNION += \
+    file.te \
+    file_contexts \
+    fs_use \
+    genfs_contexts \
+    installd.te \
+    seapp_contexts \
+    vold.te \
     mac_permissions.xml
