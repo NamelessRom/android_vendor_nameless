accbf9fb9643b8e7b8561cba37b65a6a8aaa3076
jumoog <kilianvonpflugk@gmail.com>
2014-02-02 23:02:25 +0100

    We need only Time between Yesterday 02:00 - and Today 02:00
    Change-Id: Ib814e146dc0db7b46634794b05e98314b34eee6e

--- tools/changelog_creator.sh
+++ tools/changelog_creator.sh
@@ -25,8 +25,6 @@ if [ -z $1 ] || [ -z $2 ]; then
     exit 1
 fi
 
-sdate=`date --date yesterday +"%m/%d/%Y"`
-cdate=`date +"%m_%d_%Y"`
 rdir=`pwd`
 if [[ $2 == "./" ]]; then
     ddir=$rdir
@@ -40,7 +38,7 @@ find $rdir -path $rdir/.repo -prune -o -name .git -print | sed 's/\/.git//g' | s
 do
     cd $line
     # Test to see if the repo needs to have a changelog written.
-    log=$(git log --pretty="%h - %an - %s" --no-merges --since=$sdate --date-order)
+    log=$(git log --pretty="%h - %an - %s" --no-merges --since="yesterday 02:00:00" --before="today 02:00:00" --date-order)
     project=$(git remote -v | head -n1 | awk '{print $2}' | sed 's/.*\///' | sed 's/\.git//')
     if [ -z "$log" ]; then
         if [[ $3 == "-v" ]]; then
