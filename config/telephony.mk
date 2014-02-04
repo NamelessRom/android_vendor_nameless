ea3aaabd5f74ebb9af1bed0a2d8aa253b1c8ae4e
Shareef Ali <shareefalis@cyanogenmod.org>
2014-02-04 15:37:59 -0500

    WorldPhone Support
    Credits CM // Modified for NamelessROM by Jake Whatley
    
    Notes:
    
    - ALL devices that are attached to a mobile carrier (devices with telephony)
      will need to inherit "common_phone.mk"
      - common_phone.mk will inherit APNs/SPNs, Mms, and CellBroadcastReceiver
    
    - Wifi-Only devices (tablets) need to inherit common_tablet.mk
      - common_tablet.mk will NOT inherit ANYTHING that can't be used without mobile 
    carrier
        - CellBroadcastReceiver
        - APNs/SPNs
    
    ALL DEVICES WITH TELEPHONY MUST INHERIT THE FOLLOWING IN THEIR DEVICE CONFIG:
    
    - full_base_telephony.mk
    
    Wifi-Only devices should NOT inherit full_base_telephony.mk, they should rather 
    
    inherit:
    
    full_base.mk
    
    Change-Id: I421b002d160bf5f410b93e3449bf2ff54d0a9297

---
+++ config/telephony.mk
@@ -0,0 +1,11 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/nameless/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/nameless/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    CellBroadcastReceiver
