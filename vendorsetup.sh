44f24317399f2eec792e6a16e36941142b0ac29d
Jake Whatley <jake@nameless-rom.org>
2014-02-17 15:40:00 -0500

    [WIP] make g2 universal
    Change-Id: I9ae4aff00bb8cd37b884c3b4c52ad0e48231c210

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,5 @@
-#D
-add_lunch_combo nameless_d800-userdebug
-add_lunch_combo nameless_d801-userdebug
-add_lunch_combo nameless_d802-userdebug
-add_lunch_combo nameless_d803-userdebug
 #G
+add_lunch_combo nameless_g2-userdebug
 add_lunch_combo nameless_galaxysmtd-userdebug
 #H
 add_lunch_combo nameless_hammerhead-userdebug
@@ -12,12 +8,8 @@ add_lunch_combo nameless_i9100-userdebug
 add_lunch_combo nameless_i9300-userdebug
 #J
 add_lunch_combo nameless_jflte-userdebug
-#L
-add_lunch_combo nameless_ls980-userdebug
 #N
 add_lunch_combo nameless_n7000-userdebug
 #P
 add_lunch_combo nameless_p970-userdebug
 add_lunch_combo nameless_p880-userdebug
-#V
-add_lunch_combo nameless_vs980-userdebug
