21a0d77399b78869234a99d38322d5977df1c98b
jumoog <kilianvonpflugk@gmail.com>
2014-05-21 10:59:36 +0200

    add haida device
    also fix order
    
    Change-Id: I22013d13d567074925c97028288d5cde628d701e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,7 @@ add_lunch_combo nameless_d803-userdebug
 #G
 add_lunch_combo nameless_galaxysmtd-userdebug
 #H
+add_lunch_combo nameless_haida-userdebug
 add_lunch_combo nameless_hammerhead-userdebug
 #I
 add_lunch_combo nameless_i9100-userdebug
@@ -21,7 +22,7 @@ add_lunch_combo nameless_ls980-userdebug
 #N
 add_lunch_combo nameless_n7000-userdebug
 #P
-add_lunch_combo nameless_p970-userdebug
 add_lunch_combo nameless_p880-userdebug
+add_lunch_combo nameless_p970-userdebug
 #V
 add_lunch_combo nameless_vs980-userdebug
