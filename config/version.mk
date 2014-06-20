188f559b5a2d96c21e5087da247b770d2dfd08ef
beegee-tokyo <bernd@giesecke.tk>
2014-06-20 19:50:28 +0900

    Using local date  for ro.nameless.date and UTC for the file name can lead to different dates
    Change-Id: I1727073e2206997c82e18c17b15c0a42fbc8f3de

--- config/version.mk
+++ config/version.mk
@@ -21,7 +21,7 @@ else
 ifeq ($(ROM_BUILDTIME_LOCAL),y)
 	ROM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d-%H%M%z)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
 else
-	ROM_VERSION := $(PLATFORM_VERSION)-$(shell date -u +%Y%m%d)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
+	ROM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
 endif
 endif
 
