923e6918712bcb039a9f37a6bcaebbcb8958c3ec
Evisceration <eviscerationls@gmail.com>
2014-06-21 21:35:22 +0200

    tools: update changelog creator
    Change-Id: I143829934fd5e27b17d2e541a87e64544adfd745

--- tools/changelog_creator.sh
+++ tools/changelog_creator.sh
@@ -29,15 +29,17 @@ fi
 
 rdir=$(pwd)
 if [[ $2 == "./" ]]; then
-    ddir=$rdir
+    ddir=${rdir}
 else
-    ddir=$2
+    ddir=${2}
 fi
-if [[ -z $2 ]] || [[ $2 == "-v" ]]; then
-    outputfile=$1.changelog
+if [[ -z ${2} ]] || [[ ${2} == "-v" ]]; then
+    outputfile=${1}.changelog
 else
-    outputfile=$ddir/$1.changelog
+    outputfile=${ddir}/${1}.changelog
 fi
+
+tmpfile=${outputfile}.tmp
 
 if [[ $(date +%u) -eq 7 ]] ; then
     targetdate="1 sunday 02:00:00 ago"
@@ -45,38 +47,54 @@ else
     targetdate="yesterday 02:00:00"
 fi
 
-# Clean outputfile
-rm -f $outputfile
+changes_since='changes since '${targetdate}
+update_name=${outputfile}
+update_md5=$( cut -d ' ' -f 1 <<< $(md5sum ${update_name}))
 
-#Header
-echo '<?xml version="1.0" encoding="utf-8"?>' >> $outputfile
-echo '<changelog bulletedList="true">' >> $outputfile
+# Clean outputfile
+rm -f ${outputfile}
+rm -f ${tmpfile}
+
+# Head
+echo -n '<!DOCTYPE html><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><title>NamelessRom - Changelog</title><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"><link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css"></head>' >> ${outputfile}
+# Body
+echo -n '<body><div class="container"><h1>NamelessRom&nbsp;<small>'${changes_since}'</small></h1><h4 style="padding-top:10px;padding-bottom:20px;">'${update_name}'&nbsp;<small>'${update_md5}'</small></h4><ul class="list-group">' >> ${outputfile}
 
 # Find the directories to log
-find $rdir -path $rdir/.repo -prune -o -name .git -print | sed 's/\/.git//g' | sed 'N;$!P;$!D;$d' | while read line
+find ${rdir} -path ${rdir}/.repo -prune -o -name .git -print | sed 's/\/.git//g' | sed 'N;$!P;$!D;$d' | while read line
 do
-    cd $line
+    cd ${line}
     # Test to see if the repo needs to have a changelog written.
-    log=$(git log --pretty="%s" --no-merges --since="${targetdate}" --date-order)
+    log=$(git log --no-merges --date-order --oneline --since="${targetdate}")
     project=$(git remote -v | head -n1 | awk '{print $2}' | sed 's/.*\///' | sed 's/\.git//')
-    if [ -z "$log" ]; then
+    if [ -z "${log}" ]; then
         if [[ $2 == "-v" ]] || [[ $3 == "-v" ]]; then
-            printf "\033[31m[-] Nothing updated on $project, skipping\033[0m\n"
+            printf "\033[31m[-] Nothing updated on ${project}, skipping\033[0m\n"
         fi
     else
-        printf "\033[32m[+] Writing changes for $project\033[0m\n"
+        printf "\033[32m[+] Writing changes for ${project}\033[0m\n"
 
-        # Write the changelog
-        echo '<changelogversion versionName="'${project}'">' >> $outputfile
+        echo -n "0" > ${tmpfile}counter
         echo "$log" | while read line
         do
-            echo '    <changelogtext><![CDATA['$line']]></changelogtext>' >> $outputfile
+            change_count=$(cat ${tmpfile}counter)
+            let change_count+=1
+            splitted=' ' read -r change_id change_title <<< ${line}
+            echo -n '<li class="'${project}'-content list-group-item hidden" style="background-color:#f7f7f9;margin-left:10px;"><a href="https://github.com/NamelessRom/'${project}'/commit/'${change_id}'"><b>'${change_id}'</b></a>&nbsp;-&nbsp;<em>'${change_title}'</em></li>' >> ${tmpfile}content
+            echo -n ${change_count} > ${tmpfile}counter
         done
-        echo "</changelogversion>" >> $outputfile
+
+        change_count=$(cat ${tmpfile}counter)
+        rm -f ${tmpfile}counter
+        content=$(cat ${tmpfile}content)
+        rm -f ${tmpfile}content
+
+        echo -n '<li id="'${project}'-main" class="toggleable list-group-item show"><span class="badge">'${change_count}'</span><span class="caret"></span>&nbsp; '${project}'</li>' >> ${outputfile}
+        echo -n ${content} >> ${outputfile}
     fi
 done
 
-#Footer
-echo '</changelog>' >> $outputfile
+# Finish it
+echo -n '</ul></div><script type="text/javascript" src="./js/jquery.min.js"></script><script type="text/javascript" src="./js/main.js"></script></body></html>' >> ${outputfile}
 
 exit 0
