5aca0bdcbf33fdf9bb2bf99928edf96de1f80f5a
jumoog <kilianvonpflugk@gmail.com>
2014-07-17 19:15:31 +0200

    change find7a to find7u
    Change-Id: Ie8cfa436b37abed74cc15228f5624d7b01793e7a

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,7 +9,7 @@ add_lunch_combo nameless_d803-userdebug
 add_lunch_combo nameless_dlx-userdebug
 #F
 add_lunch_combo nameless_find7-userdebug
-add_lunch_combo nameless_find7a-userdebug
+add_lunch_combo nameless_find7u-userdebug
 #G
 add_lunch_combo nameless_galaxysl-userdebug
 add_lunch_combo nameless_galaxysmtd-userdebug
