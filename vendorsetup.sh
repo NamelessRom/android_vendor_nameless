f7e2223f8f36cd808a5c58a71af5d1ebef1cd1b8
Eslam Mohamed <love3andala@gmail.com>
2014-07-26 22:37:05 +0200

    Support all Xperia 2011 devices
    Change-Id: I3e4a850ce9656b198283d9206f33b2fbcb20886a

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,7 @@
 #A
 add_lunch_combo nameless_anzu-userdebug
+#C
+add_lunch_combo nameless_coconut-userdebug
 #D
 add_lunch_combo nameless_d2lte-userdebug
 add_lunch_combo nameless_d800-userdebug
@@ -16,18 +18,21 @@ add_lunch_combo nameless_galaxysmtd-userdebug
 add_lunch_combo nameless_grouper-userdebug
 #H
 add_lunch_combo nameless_haida-userdebug
+add_lunch_combo nameless_hallon-userdebug
 add_lunch_combo nameless_hammerhead-userdebug
 #I
 add_lunch_combo nameless_i9082-userdebug
 add_lunch_combo nameless_i9100-userdebug
 add_lunch_combo nameless_i9300-userdebug
 add_lunch_combo nameless_i9500-userdebug
+add_lunch_combo nameless_iyokan-userdebug
 #J
 add_lunch_combo nameless_jflte-userdebug
 #L
 add_lunch_combo nameless_ls980-userdebug
 #M
 add_lunch_combo nameless_mako-userdebug
+add_lunch_combo nameless_mango-userdebug
 #N
 add_lunch_combo nameless_n7000-userdebug
 add_lunch_combo nameless_nicki-userdebug
@@ -39,7 +44,12 @@ add_lunch_combo nameless_p765-userdebug
 add_lunch_combo nameless_p768-userdebug
 add_lunch_combo nameless_p880-userdebug
 add_lunch_combo nameless_p970-userdebug
+#S
+add_lunch_combo nameless_satsuma-userdebug
+add_lunch_combo nameless_smultron-userdebug
 #T
 add_lunch_combo nameless_tilapia-userdebug
+#U
+add_lunch_combo nameless_urushi-userdebug
 #V
 add_lunch_combo nameless_vs980-userdebug
