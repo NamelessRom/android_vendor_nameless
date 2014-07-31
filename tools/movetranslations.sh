a8dd6c6171c32306b46df6e2a31afcc96958f5f2
Evisceration <eviscerationls@gmail.com>
2014-07-31 19:37:03 +0200

    update translation tool for namelesscenter
    Change-Id: Ib6ff8352fb144974c7a2804110a07e47b7ebb76c

--- tools/movetranslations.sh
+++ tools/movetranslations.sh
@@ -16,7 +16,7 @@ core=${base}/frameworks/base/core/res/res/values
 devicecontrol=${base}/packages/apps/DeviceControl/resources/src/main/res/values
 settings=${base}/packages/apps/Settings/res/values
 systemui=${base}/frameworks/base/packages/SystemUI/res/values
-updatecenter=${base}/packages/apps/UpdateCenter/src/main/res/values
+namelesscenter=${base}/packages/apps/NamelessCenter/app/src/main/res/values
 ###########################################################################
 . ${base}/build/envsetup.sh
 ###########################################################################
@@ -38,9 +38,9 @@ for i in ${!languages[*]}; do
     mkdir -p ${systemui}-${values[$i]}/
     cp ${languages[$i]}/SystemUI/*.xml ${systemui}-${values[$i]}/
     #######################################################################
-    echo "${languages[$i]}: [UpdateCenter]"
-    mkdir -p ${updatecenter}-${values[$i]}/
-    cp ${languages[$i]}/UpdateCenter/*.xml ${updatecenter}-${values[$i]}/
+    echo "${languages[$i]}: [NamelessCenter]"
+    mkdir -p ${namelesscenter}-${values[$i]}/
+    cp ${languages[$i]}/NamelessCenter/*.xml ${namelesscenter}-${values[$i]}/
     #######################################################################
 done
 ###########################################################################
@@ -60,8 +60,8 @@ git add res/
 git commit -m "automatic translation import"
 gerritupload ${username}
 ###########################################################################
-cd ${base}/packages/apps/UpdateCenter/
-git add src/main/res/
+cd ${base}/packages/apps/NamelessCenter/
+git add app/src/main/res/
 git commit -m "automatic translation import"
 gerritupload ${username}
 ###########################################################################
