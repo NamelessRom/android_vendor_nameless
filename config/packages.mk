ecfa31796ea5c7d5982199a9cc90df7fdbe45b2c
Alexander Martinz <eviscerationls@gmail.com>
2014-08-26 13:21:32 +0200

    Revert "(2/2) vendor: use trebuchet"
    This reverts commit a54354de1a3b3dd65feb307a8be3cd4049083fdf.
    
    Change-Id: I7eb310b41222f5b225006dccda4b4d9040f9d395

--- config/packages.mk
+++ config/packages.mk
@@ -9,14 +9,14 @@ PRODUCT_PACKAGES += \
 # Additional apps
 PRODUCT_PACKAGES += \
     Apollo \
+    CustomLauncher3 \
     DashClock \
     DeviceControl \
     Flashlight \
     NamelessCenter \
     NamelessProvider \
     NamelessSetupWizard \
-    SuperSU \
-    Trebuchet \
+    SuperSU
 
 # DSP Manager
 ifneq ($(USE_DSP_MANAGER),false)
