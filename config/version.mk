39aa498c1e3281f996b64379c21b4a82bff601df
Evisceration <eviscerationls@gmail.com>
2014-09-03 16:09:54 +0200

    update rom versioning
    Change-Id: I5dbb0e0ea4a1aa4585a9675a4d24b37941c2a6a7

--- config/version.mk
+++ config/version.mk
@@ -1,14 +1,21 @@
 # Versioning of the ROM
 
 ifeq ($(BUILD_IS_JENKINS),1)
-	ROM_BUILDTYPE := NIGHTLY
+
+ifneq ($(BUILD_BUILDTYPE),)
+    ROM_BUILDTYPE = $(BUILD_BUILDTYPE)
+else
+    ROM_BUILDTYPE := NIGHTLY
 endif
+
+endif # we are jenkins
+
 ifdef BUILDTYPE_RELEASE
-	ROM_BUILDTYPE := RELEASE
+    ROM_BUILDTYPE := RELEASE
 endif
 
 ifndef ROM_BUILDTYPE
-	ROM_BUILDTYPE := HOMEMADE
+    ROM_BUILDTYPE := HOMEMADE
 endif
 
 TARGET_PRODUCT_SHORT := $(TARGET_PRODUCT)
@@ -16,25 +23,26 @@ TARGET_PRODUCT_SHORT := $(subst nameless_,,$(TARGET_PRODUCT_SHORT))
 
 # Build the final version string
 ifdef BUILDTYPE_RELEASE
-	ROM_VERSION := $(PLATFORM_VERSION)-$(TARGET_PRODUCT_SHORT)
+    ROM_VERSION := $(PLATFORM_VERSION)-$(TARGET_PRODUCT_SHORT)
 else
 ifneq ($(ROM_BUILDTIME_CUSTOM),)
-	DATE := $(ROM_BUILDTIME_CUSTOM)
+    DATE := $(ROM_BUILDTIME_CUSTOM)
 else
 ifeq ($(ROM_BUILDTIME_LOCAL),y)
-	DATE := $(shell date +%Y%m%d-%H%M%z)
+    DATE := $(shell date +%Y%m%d-%H%M%z)
 else
-	DATE := $(shell date -u +%Y%m%d)
+    DATE := $(shell date -u +%Y%m%d)
 endif
 endif # BUILDTYPE_RELEASE
 
-ROM_VERSION := $(PLATFORM_VERSION)-$(DATE)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
+ROM_VERSION := $(PLATFORM_VERSION)-$(DATE)-$(TARGET_PRODUCT_SHORT)
 endif
 
 NAMELESS_VERSION := $(ROM_VERSION)
 
 # Apply it to build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.modversion=NamelessROM-$(ROM_VERSION) \
-	ro.nameless.version=$(ROM_VERSION) \
-	ro.nameless.date=$(shell date +"%Y%m%d")
+    ro.modversion=NamelessRom-$(ROM_VERSION) \
+    ro.nameless.version=$(ROM_VERSION) \
+    ro.nameless.date=$(shell date +"%Y%m%d") \
+    ro.nameless.releasetype=$(ROM_BUILDTYPE) \
