76a43d15fefcc8d92ddc5772d920e5e033ed8e06
Evisceration <eviscerationls@gmail.com>
2014-09-18 16:15:53 +0200

    update apns
    Change-Id: I678f06596af3481d6dad20a47bc60c57d2560e03

--- config/apns.mk
+++ config/apns.mk
@@ -1,3 +1,11 @@
-# APN list
+# World APN list
 PRODUCT_COPY_FILES += \
     vendor/nameless/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/nameless/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/nameless/prebuilt/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
