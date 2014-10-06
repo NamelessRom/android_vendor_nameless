98747235a21b7562f189574381a52941cfc7c895
Evisceration <eviscerationls@gmail.com>
2014-10-06 19:44:20 +0200

    (1/2) add support for f2fs
    mkbootimg and unpackbootimg got pre compiled with issuing:
      mmm system/core/mkbootimg/
    
    Change-Id: I807a13b71fc7de060cc6fde3e51cb7904c45cdb7

---
+++ prebuilt/bin/f2fs/trigger.sh
@@ -0,0 +1,10 @@
+#!/sbin/sh
+
+if [ -e /tmp/post_installation_extras/f2fs.sh ]; then
+    mount | grep -q 'type f2fs'
+    F2FS=$?
+    if [ $F2FS -eq 0 ]; then
+        chmod 777 /tmp/post_installation_extras/f2fs.sh
+        /tmp/post_installation_extras/f2fs.sh
+    fi
+fi
