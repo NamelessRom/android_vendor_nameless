cd841f6ef08c50c76a77f8a7717b97154f3dc8d3
jumoog <kilianvonpflugk@gmail.com>
2014-10-24 18:22:12 +0200

    add supersuflasher.sh
    Change-Id: Id8261d7bb0554c359879eef5eaf6bed914f69245

---
+++ prebuilt/bin/supersuflasher.sh
@@ -0,0 +1,9 @@
+#!/sbin/sh
+mkdir -p /tmp/supersu_1/
+unzip -o -q /system/supersu.zip -d /tmp/supersu_1/
+chmod 755 /tmp/supersu_1/META-INF/com/google/android/update-binary
+sh /tmp/supersu_1/META-INF/com/google/android/update-binary 2 20 /system/supersu.zip
+rm -rf /system/supersu.zip
+#shut up cwm recovery
+ln -s /system/xbin/su /system/bin/su
+touch /system/etc/.installed_su_daemon
