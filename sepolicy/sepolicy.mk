ac206ca2d9c9d5099133c5def3919f1d69ef4593
Evisceration <eviscerationls@gmail.com>
2014-11-10 15:37:56 +0100

    sepolicy: update
    Change-Id: I1ef173dd722bcf2c49c20d4a8b030ef402de0e6f

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -9,10 +9,11 @@ BOARD_SEPOLICY_DIRS += \
 BOARD_SEPOLICY_UNION += \
     file.te \
     file_contexts \
-    fs_use \
     genfs_contexts \
     seapp_contexts \
     installd.te \
+    netd.te \
+    system.te \
     ueventd.te \
     vold.te \
     mac_permissions.xml
