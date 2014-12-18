cb5c681556cc41d55587d71f4116388f992770f8
Evisceration <eviscerationls@gmail.com>
2014-12-18 17:13:11 +0100

    vendorsetup: comment out unsupported devices
    Change-Id: Ie3cd6b27ce1a8b0ff28d0d141fbf4779fadbfa82

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,50 +1,51 @@
-#A
-add_lunch_combo nameless_anzu-userdebug
-#B
+## A
+#add_lunch_combo nameless_anzu-userdebug
+## B
 add_lunch_combo nameless_bacon-userdebug
-#D
-add_lunch_combo nameless_d2lte-userdebug
-add_lunch_combo nameless_d800-userdebug
-add_lunch_combo nameless_d801-userdebug
-add_lunch_combo nameless_d802-userdebug
-add_lunch_combo nameless_d803-userdebug
-add_lunch_combo nameless_dlx-userdebug
-#F
-add_lunch_combo nameless_find7-userdebug
-add_lunch_combo nameless_find7u-userdebug
+## D
+#add_lunch_combo nameless_d2lte-userdebug
+#add_lunch_combo nameless_d800-userdebug
+#add_lunch_combo nameless_d801-userdebug
+#add_lunch_combo nameless_d802-userdebug
+#add_lunch_combo nameless_d803-userdebug
+#add_lunch_combo nameless_dlx-userdebug
+## F
 add_lunch_combo nameless_find7a-userdebug
 add_lunch_combo nameless_find7au-userdebug
-#G
-add_lunch_combo nameless_galaxysl-userdebug
-add_lunch_combo nameless_galaxysmtd-userdebug
-add_lunch_combo nameless_grouper-userdebug
-#H
-add_lunch_combo nameless_haida-userdebug
+add_lunch_combo nameless_find7s-userdebug
+add_lunch_combo nameless_find7su-userdebug
+## G
+#add_lunch_combo nameless_galaxysl-userdebug
+#add_lunch_combo nameless_galaxysmtd-userdebug
+#add_lunch_combo nameless_grouper-userdebug
+## H
+#add_lunch_combo nameless_haida-userdebug
 add_lunch_combo nameless_hammerhead-userdebug
-#I
-add_lunch_combo nameless_i9082-userdebug
-add_lunch_combo nameless_i9100-userdebug
+## I
+#add_lunch_combo nameless_i9082-userdebug
+#add_lunch_combo nameless_i9100-userdebug
 add_lunch_combo nameless_i9300-userdebug
-add_lunch_combo nameless_i9500-userdebug
-#J
-add_lunch_combo nameless_jflte-userdebug
-#L
-add_lunch_combo nameless_ls980-userdebug
-#M
-add_lunch_combo nameless_mako-userdebug
-add_lunch_combo nameless_moto_msm8960
-#N
-add_lunch_combo nameless_n7000-userdebug
-add_lunch_combo nameless_nicki-userdebug
-#P
-add_lunch_combo nameless_p4-userdebug
-add_lunch_combo nameless_p4wifi-userdebug
-add_lunch_combo nameless_p760-userdebug
-add_lunch_combo nameless_p765-userdebug
-add_lunch_combo nameless_p768-userdebug
-add_lunch_combo nameless_p880-userdebug
-add_lunch_combo nameless_p970-userdebug
-#T
-add_lunch_combo nameless_tilapia-userdebug
-#V
-add_lunch_combo nameless_vs980-userdebug
+#add_lunch_combo nameless_i9500-userdebug
+## J
+#add_lunch_combo nameless_jflte-userdebug
+## L
+#add_lunch_combo nameless_ls980-userdebug
+## M
+#add_lunch_combo nameless_mako-userdebug
+#add_lunch_combo nameless_moto_msm8960
+## N
+#add_lunch_combo nameless_n7000-userdebug
+add_lunch_combo nameless_n7100-userdebug
+#add_lunch_combo nameless_nicki-userdebug
+## P
+#add_lunch_combo nameless_p4-userdebug
+#add_lunch_combo nameless_p4wifi-userdebug
+#add_lunch_combo nameless_p760-userdebug
+#add_lunch_combo nameless_p765-userdebug
+#add_lunch_combo nameless_p768-userdebug
+#add_lunch_combo nameless_p880-userdebug
+#add_lunch_combo nameless_p970-userdebug
+## T
+#add_lunch_combo nameless_tilapia-userdebug
+## V
+#add_lunch_combo nameless_vs980-userdebug
