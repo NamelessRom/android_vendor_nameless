e6de1f15e9860e361b9d66eae916c4ce5de1a83d
Alexander Martinz <eviscerationls@gmail.com>
2015-07-16 10:37:46 +0200

    devices: add shieldtablet
    Change-Id: I4058f57dacafb3906e0a4d58053f2bd0cbb2beb2
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -43,6 +43,8 @@ add_lunch_combo nameless_n3-userdebug
 #add_lunch_combo nameless_p768-userdebug
 add_lunch_combo nameless_p880-userdebug
 #add_lunch_combo nameless_p970-userdebug
+## S
+add_lunch_combo nameless_shieldtablet-userdebug
 ## T
 #add_lunch_combo nameless_tilapia-userdebug
 ## V
