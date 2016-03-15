e9b8881f1032473e4b37636c7fa22289598e1406
Adnan Begovic <adnan@cyngn.com>
2016-03-15 14:20:37 -0700

    cm: Introduce statusbar feature xml.
    TICKET: CYNGNOS-2289
    Change-Id: I1906717f69c5123d2d3f12e860e729810073a01e

--- config/features.mk
+++ config/features.mk
@@ -4,11 +4,9 @@ PRODUCT_COPY_FILES += \
 
 # and we do support cm features
 PRODUCT_COPY_FILES += \
-    vendor/nameless/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml
-
-# Live lockscreen
-PRODUCT_COPY_FILES += \
-    vendor/nameless/config/permissions/org.cyanogenmod.livelockscreen.xml:system/etc/permissions/org.cyanogenmod.livelockscreen.xml
+    vendor/nameless/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml \
+    vendor/nameless/config/permissions/org.cyanogenmod.livelockscreen.xml:system/etc/permissions/org.cyanogenmod.livelockscreen.xml \
+    vendor/nameless/config/permissions/org.cyanogenmod.statusbar.xml:system/etc/permissions/org.cyanogenmod.statusbar.xml \
 
 # Enable SIP and VoIP on all targets
 PRODUCT_COPY_FILES += \
